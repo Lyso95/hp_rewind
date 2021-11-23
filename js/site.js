d4652c5e879357dec60ae5b80ef8e9bf146f1046
Lyso95 <amos@langensiepen.net>
2021-11-23 03:52:05 +0100

    code_finish_without_codeexplain

--- js/site.js
+++ js/site.js
@@ -1,4 +1,30 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World");
+//Get the string from the page
+//controller
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;  
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+//Reverse the string
+//model
+function reverseString(userString) {
+    let revString = [];
+    
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+
+    return revString;
+}
+
+//Display the reversed string to the user
+//view
+function displayString(revString){
+    //write to the page
+    document.getElementById("msg").innerHTML = `Dein Text rückwärts lautet: ${revString}`; 
+    //turn on alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
